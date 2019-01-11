19aa64b2de2179cedaaba6f6533d947cdd466c29
EKumi <www.estee7000@gmail.com>
2019-01-11 16:42:24 +0100

    Adding palindrome.py

---
+++ Palindrome.py
@@ -0,0 +1,10 @@
+word1=str(input("Please enter a word here:"))
+word2= word1[::-1]
+
+if word1==word2:
+ print("This word is a palindrome")
+else:
+ print("This word is not a palindrome")
+
+print("Finished")
+print("That was fun! Try another word")
